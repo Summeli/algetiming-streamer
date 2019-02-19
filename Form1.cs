3c6c016004951c67a5e6cc93294b9e4ef0dc9db5
Antti Pohjola <summeli@summeli.com>
2019-02-19 21:42:39 +0200

    reading the data from timmy device, and streaming it.

--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,131 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Diagnostics;
+using System.Windows.Forms;
 
 namespace algetiming_streamer
 {
     public partial class Form1 : Form
     {
+        Alge.TimyUsb timyUsb;
+        ClientWebSocket webSocket;
+       // Timer keepaliveTimer;
+
+        public static int SERVERKEEPALIVE = 60 * 1000;
+
         public Form1()
         {
             InitializeComponent();
+            webSocket = new ClientWebSocket();
+            webSocket.ConnectAsync(new Uri("ws://127.0.0.1:8080/time"), CancellationToken.None);
+
+            /*
+            keepaliveTimer = new Timer();
+            keepaliveTimer.Interval = SERVERKEEPALIVE; // 45 mins
+            keepaliveTimer.Tick += new EventHandler(keepaliveTimer_Tick);
+            keepaliveTimer.Start();*/
         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timyUsb = new Alge.TimyUsb(this);
+            timyUsb.Start();
+
+
+
+            timyUsb.DeviceConnected += new EventHandler<Alge.DeviceChangedEventArgs>(timyUsb_DeviceConnected);
+            timyUsb.DeviceDisconnected += new EventHandler<Alge.DeviceChangedEventArgs>(timyUsb_DeviceDisconnected);
+            //I'm only reading the line, since it's enough
+            timyUsb.LineReceived += new EventHandler<Alge.DataReceivedEventArgs>(timyUsb_LineReceived);
+            //timyUsb.BytesReceived += timyUsb_BytesReceived;
+            // timyUsb.RawReceived += new EventHandler<Alge.DataReceivedEventArgs>(timyUsb_RawReceived);
+            //timyUsb.PnPDeviceAttached += new EventHandler(timyUsb_PnPDeviceAttached);
+            //timyUsb.PnPDeviceDetached += new EventHandler(timyUsb_PnPDeviceDetached);
+            //timyUsb.HeartbeatReceived += new EventHandler<Alge.HeartbeatReceivedEventArgs>(timyUsb_HeartbeatReceived);
+
+            AddLogLine("Process is " + (IntPtr.Size == 8 ? "x64" : "x86"));
+        }
+
+        private void keepaliveTimer_Tick(object sender, EventArgs e)
+        {
+            var message = "PING";
+            byte[] sendBody = Encoding.UTF8.GetBytes(message);
+            webSocket.SendAsync(new ArraySegment<byte>(sendBody), WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+
+        void timyUsb_BytesReceived(object sender, Alge.BytesReceivedEventArgs e)
+        {
+            AddLogLine("Device " + e.Device.Id + " Bytes: " + e.Data.Length);
+        }
+
+        void timyUsb_HeartbeatReceived(object sender, Alge.HeartbeatReceivedEventArgs e)
+        {
+            AddLogLine("Heartbeat: " + e.Time.ToString());
+        }
+
+        void timyUsb_PnPDeviceDetached(object sender, EventArgs e)
+        {
+            AddLogLine("Device detached");
+        }
+
+        void timyUsb_PnPDeviceAttached(object sender, EventArgs e)
+        {
+            AddLogLine("Device attached");
+        }
+
+        void timyUsb_RawReceived(object sender, Alge.DataReceivedEventArgs e)
+        {
+
+                AddLogLine("Device " + e.Device.Id + " Raw: " + e.Data);
+
+        }
+
+        void timyUsb_LineReceived(object sender, Alge.DataReceivedEventArgs e)
+        {
+            AddLogLine("Device " + e.Device.Id + " Line: " + e.Data);
+
+            //send message to the server
+            var message = "Message " + e.Data;
+            byte[] sendBody = Encoding.UTF8.GetBytes(message);
+            webSocket.SendAsync(new ArraySegment<byte>(sendBody), WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+
+        private void ProcessProgramResponse(string line)
+        {
+            String programString = line.Substring(6);
+            AddLogLine("Active program: '" + programString + "'");
+        }
+
+        void timyUsb_DeviceDisconnected(object sender, Alge.DeviceChangedEventArgs e)
+        {
+            AddLogLine("Device " + e.Device.Id + " disconnected, " + timyUsb.ConnectedDevicesCount + " total connected");
+        }
+
+        void timyUsb_DeviceConnected(object sender, Alge.DeviceChangedEventArgs e)
+        {
+            AddLogLine("Device " + e.Device.Id + " connected, " + timyUsb.ConnectedDevicesCount + " total connected");
+        }
+
+        void AddLogLine(String str)
+        {
+            Debug.WriteLine(str);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timyUsb.DeviceConnected -= new EventHandler<Alge.DeviceChangedEventArgs>(timyUsb_DeviceConnected);
+            timyUsb.DeviceDisconnected -= new EventHandler<Alge.DeviceChangedEventArgs>(timyUsb_DeviceDisconnected);
+            timyUsb.LineReceived -= new EventHandler<Alge.DataReceivedEventArgs>(timyUsb_LineReceived);
+            //timyUsb.BytesReceived -= timyUsb_BytesReceived;
+            //timyUsb.RawReceived -= new EventHandler<Alge.DataReceivedEventArgs>(timyUsb_RawReceived);
+            //timyUsb.PnPDeviceAttached -= new EventHandler(timyUsb_PnPDeviceAttached);
+            //timyUsb.PnPDeviceDetached -= new EventHandler(timyUsb_PnPDeviceDetached);
+            //timyUsb.HeartbeatReceived -= new EventHandler<Alge.HeartbeatReceivedEventArgs>(timyUsb_HeartbeatReceived);
+        }
+
+
+ 
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/***************************************************************
+ * 
+ *   Copyright 2019 Antti Pohjola
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  
+ ***************************************************************/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +33,62 @@ namespace algetiming_streamer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            // Attach an event handler for missing assemblies
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
+
             Application.Run(new Form1());
         }
+        #region x86/x64 compatibility
+
+        public static System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            // Check for if Alge Timy assembly needs to be resolved
+            if (args.Name.ToLower().Contains("algetimyusb.dummy"))
+            {
+                // Detach event handler as it is not needed anymore
+                AppDomain.CurrentDomain.AssemblyResolve -= new ResolveEventHandler(CurrentDomain_AssemblyResolve);
+
+                // Construct correct filename depending on the platform
+                String filename = "AlgeTimyUsb." + (IsX64Process ? "x64" : "x86") + ".dll";
+                var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+                filename = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(assembly.Location), filename);
+
+                // Check for file exists
+                if (System.IO.File.Exists(filename))
+                {
+                    try
+                    {
+                        // Try to load assembly
+                        var a = System.Reflection.Assembly.LoadFile(filename);
+                        return a;
+                    }
+                    catch (Exception ex)
+                    {
+                        // Error on loading assembly - Timy Usb will not work and application will crash
+                        System.Windows.Forms.MessageBox.Show("Error on loading the existing file '" + System.IO.Path.GetFileName(filename) + "'. This application cannot be executed without it.\nPlease make sure that you have Microsoft Visual C++ 2012 Runtime installed.\n\n" + ex.Message, "Error", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Warning);
+
+                    }
+                }
+                else
+                {
+                    // Correct assembly for platform does not exist - Timy Usb will not work and application will crash
+                    System.Windows.Forms.MessageBox.Show("Unable to find " + System.IO.Path.GetFileName(filename) + ". This application cannot be executed without it.", "Error", System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Warning);
+                }
+                return null;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Determines whether the process is running as x64 or x86
+        /// </summary>
+        public static bool IsX64Process
+        {
+            get { return IntPtr.Size == 8; }
+        }
+
+        #endregion
+
     }
 }
